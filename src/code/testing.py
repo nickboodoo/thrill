82ee5d70a95a30e935e8ae5c7d791d14f8a031e5
Nick <boodoo@temple.edu>
2024-04-30 01:10:39 -0400

    modified prime number game to include perfect numbers

--- src/code/testing.py
+++ src/code/testing.py
@@ -1,26 +1,31 @@
 def prime_factors(n):
     factors = []
-    # Check for number of 2s that divide n
     while n % 2 == 0:
         factors.append(2)
         n = n // 2
 
-    # n must be odd at this point, thus skip even numbers
     for i in range(3, int(n**0.5) + 1, 2):
-        # While i divides n, add i and divide n
         while n % i == 0:
             factors.append(i)
             n = n // i
 
-    # This condition is to check if n is a prime number greater than 2
     if n > 2:
         factors.append(n)
 
     return factors
 
+def is_perfect_number(n):
+    sum_divisors = 1
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            sum_divisors += i
+            if i != n // i:
+                sum_divisors += n // i
+    return sum_divisors == n
+
 def main():
     try:
-        number = int(input("Enter a number to find its prime factors: "))
+        number = int(input("Enter a number to find its prime factors and check if it's a perfect number: "))
         if number <= 1:
             print("Please enter a number greater than 1.")
         else:
@@ -29,6 +34,11 @@ def main():
                 print("Prime factors of", number, "are:", factors)
             else:
                 print("No prime factors, the number is prime itself.")
+
+            if is_perfect_number(number):
+                print(number, "is a perfect number.")
+            else:
+                print(number, "is not a perfect number.")
     except ValueError:
         print("Please enter a valid integer.")
 
