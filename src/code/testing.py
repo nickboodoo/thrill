ed657484b49f4426b45dfc4c012bff0ffe83e4f3
Nick <boodoo@temple.edu>
2024-05-02 12:39:19 -0400

    optimized prime numbers

--- src/code/testing.py
+++ src/code/testing.py
@@ -1,14 +1,17 @@
 def prime_factors(n):
     factors = []
+    # Check for number of 2s that divide n
     while n % 2 == 0:
         factors.append(2)
-        n = n // 2
+        n //= 2
 
+    # n must be odd at this point, so we can skip even numbers
     for i in range(3, int(n**0.5) + 1, 2):
         while n % i == 0:
             factors.append(i)
-            n = n // i
+            n //= i
 
+    # This condition is to check if n is a prime number greater than 2
     if n > 2:
         factors.append(n)
 
